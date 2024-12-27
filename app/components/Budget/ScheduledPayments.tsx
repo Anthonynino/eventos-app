187d2a711ebe279e429de7ed0646101048fad465
Anthony Nino <anthonyninojose@gmail.com>
2024-12-27 15:49:02 -0400

    Budget Ready

---
+++ app/components/Budget/ScheduledPayments.tsx
@@ -0,0 +1,43 @@
+const ScheduleTable = () => {
+    return ( <>
+        <div className=" w-full gap-6 pt-2 md:pr-0 pb-4 h-[100vh]">
+            <div className="w-full max-w-screen-lg relative mx-auto inset-x-0 ">
+                <div className="bg-white p-6 h-max shadow-md rounded-xl mt-10 overflow-x-auto* ">
+                    <div className="w-full ">
+                    <table className="table-auto border-collapse w-full rounded-lg relative p-4 ">
+                        <thead className="relative text-xs text-gray-800 uppercase bg-gray-100 w-full">
+                            <tr role="row" className="grid grid-cols-24">
+                                <th role="columnheader" colSpan={1} className="px-10 py-1 md:py-2 text-center flex justify-center items-center text-sm font-light font-display col-span-4">
+                                        ESTADO
+                                    
+                                    <th role="columnheader" colSpan={1} className="px-10 py-1 md:py-2 text-center flex justify-center items-center text-sm font-light font-display col-span-4">
+                                        Proveedor
+                                    </th>
+                                    <th role="columnheader" colSpan={1} className="px-14 py-1 md:py-2 text-center flex justify-center items-center text-sm font-light font-display col-span-4">
+                                    Fecha de futuro pago
+                                    </th>
+                                    
+                                    <th role="columnheader" colSpan={1} className="px-14 py-1 md:py-2 text-center flex justify-center items-center text-sm font-light font-display col-span-4">
+                                        cONCEPTO
+                                    </th>
+                                
+                                </th>
+                                
+                            </tr>
+                        </thead>
+                            <tbody className="text-gray-700 text-sm ">
+                                <tr className="transition border-b border-base hover:bg-base cursor-pointer w-full grid place-items-center">
+                                    <td className="py-5 font-display text-lg text-gray-500 uppercase ">
+                                    No hay pagos asociados
+                                    </td>
+                                </tr>
+                            </tbody>
+                    </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </> );
+}
+ 
+export default ScheduleTable;
--- app/presupuesto/page.tsx
+++ app/presupuesto/page.tsx
@@ -1,10 +1,19 @@
-import { FaListUl, FaPlusCircle } from "react-icons/fa";
+"use client";
 import MinHeader from "../components/MinHeader";
 import NavbarApp from "../components/Navbar/NavbarApp";
-import LiCost from "../components/LiCost";
-import { FaCoins, FaPiggyBank } from "react-icons/fa6";
+import { useState } from "react";
+import BudgetC from "../components/Budget/BudgetC";
+import PaymentsTable from "../components/Budget/PaymentsTable";
+import ScheduleTable from "../components/Budget/ScheduledPayments";
 
 const Budget = () => {
+
+    const [activeTab, setActiveTab] = useState('presupuesto');
+
+    const handleTabClick = (tab: string) => {
+        setActiveTab(tab);
+    };
+
     return ( <>
             <div className="w-[100vw] h-[100vh]">
                 <div className="md:block relative">
@@ -16,87 +25,31 @@ const Budget = () => {
                             <div className="max-w-screen-lg mx-auto inset-x-0 px-2 md:px-0 w-full ">
                                 <MinHeader name="Rumba llaneras" category="Cumpleanos"/>
                                 <div className="md:w-96 mx-auto inset-x-0 flex my-2 mt-4 rounded-2xl overflow-hidden">
-                                    <div className="w-[40%] md:w-[270px] py-1 bg-[#f7628c] text-white h-full grid place-items-center font-display font-medium text-sm cursor-pointer hover:opacity-90 capitalize">
+                                    <div onClick={() => handleTabClick('presupuesto')} className={`w-[40%] md:w-[270px] py-1 ${activeTab === 'presupuesto' ? 'bg-[#f7628c] text-white' : 'bg-white text-[#f7628c]'} h-full grid place-items-center font-display font-medium text-sm cursor-pointer hover:opacity-90 capitalize`}>
                                         <p>
                                             Presupuesto
                                         </p>
                                     </div>
-                                    <div className="w-[25%] md:w-1/2 py-1 bg-white text-[#f7628c] h-full grid place-items-center font-display font-medium text-sm cursor-pointer hover:opacity-90 border-x-2 capitalize">
+                                    <div onClick={() => handleTabClick('pago')} className={`w-[25%] md:w-1/2 py-1 ${activeTab === 'pago' ? 'bg-[#f7628c] text-white' : 'bg-white text-[#f7628c]'} h-full grid place-items-center font-display font-medium text-sm cursor-pointer hover:opacity-90 border-x-2 capitalize`}>
                                         <p>
                                             Pago
                                         </p>
                                     </div>
-                                    <div className=" w-[40%] md:w-[320px] py-1  bg-white text-[#f7628c] h-full grid place-items-center font-display font-medium text-sm cursor-pointer hover:opacity-90">
+                                    <div onClick={() => handleTabClick('pago-programado')} className={`w-[40%] md:w-[320px] py-1  ${activeTab === 'pago-programado' ? 'bg-[#f7628c] text-white' : 'bg-white text-[#f7628c]'} h-full grid place-items-center font-display font-medium text-sm cursor-pointer hover:opacity-90`}>
                                         <p>
                                             Pagos programados
                                         </p>
                                     </div>
                                 </div>
-                                <div className="grid md:grid-cols-3 w-full gap-6 pt-2 md:pr-0 pb-4">
-                                    <div className=" bg-white w-full shadow-md rounded-xl h-max ">
-                                        <button className=" focus:outline-none bg-[#f7628c] rounded-xl font-display font-light text-md flex gap-2 w-full py-1 items-center justify-center text-white hover:scale-105 transition transform">
-                                            <FaPlusCircle className="w-4 h-4"/>
-                                            Nueba categorias
-                                        </button>
-                                        <ul className=" w-full flex flex-col font-display text-sm h-44 overflow-y-auto md:h-max divide-y text-gray-400 cursor-not-allowed*">
-                                            <LiCost/>
-                                            <LiCost/>
-                                            <LiCost/>
-                                            <LiCost/>
-                                            <LiCost/>
-                                            <LiCost/>
-
-                                        </ul>
-                                    </div>
-                                    <div className="md:col-span-2 w-full flex flex-col relative">
-                                        <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-                                            <div className=" bg-white shadow-md rounded-xl grid place-items-center p-4">
-                                                <FaPiggyBank className="w-12 h-12 text-gray-500 "/>
-                                                <p className="jsx-2ae18751a0032410 font-display text-gray-500 font-light text-md grid place-items-center">
-                                                    Presupuesto estimado
-                                                </p>
-                                                <span className=" font-display text-gray-500 font-semibold text-lg text-center">
-                                                11.00
-                                                <select name="" id="" className=" border-none focus:ring-0 cursor-pointer">
-                                                    <option value="">USD</option>
-                                                    <option value="">EUR</option>
-                                                    <option value="">COP</option>
-
-                                                </select>
-                                                </span>
-                                                <button className=" border-[#f7628c] border font-display focus:outline-none text-[#f7628c] text-xs bg-white px-3 py-1 rounded-lg my-2 hover:bg-primary hover:text-white transition">
-                                                    Modificar presupuesto
-                                                </button>
-                                            </div>
-                                            <div className=" bg-white shadow-md rounded-xl grid place-items-center p-4">
-                                                <FaCoins className="w-12 h-12 text-[#f7628c] "/>
-                                                <p className="font-display text-gray-500 font-light text-md grid place-items-center">
-                                                    Coste final <br /> <span className="font-semibold text-lg text-center">US$0.00</span>
-                                                </p>
-                                                <div className=" w-full rounded-xl overflow-hidden flex my-2">
-                                                    <div className="w-1/2 bg-[#f7628c] py-1 px-3">
-                                                        <p className="text-xs font-display text-white">
-                                                            Pagado US$0.00
-                                                        </p>
-                                                    </div>
-                                                    <div className="w-1/2 bg-[#fbff4e] py-1 px-3">
-                                                        <p className="text-xs font-display text-[#f7628c]">
-                                                            Por pagar US$0.00
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="w-full pt-2">
-                                            <h2 className="font-display pb-2 text-xl text-gray-500 font-semibold text-center w-full">
-                                                Cuanto cuesta mi evento?
-                                            </h2>
-                                            <div className=" w-full h-full md:mb-2 md:h-max bg-white rounded-xl shadow-md flex justify-center  md:py-6 pt-6  ">
-                                                <img src="https://placehold.co/600x400" alt="" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                {activeTab === 'presupuesto' && (
+                                    <BudgetC/>
+                                )}
+                                {activeTab === 'pago' && (
+                                    <PaymentsTable/>
+                                )}
+                                {activeTab === 'pago-programado' && (
+                                    <ScheduleTable/>
+                                )}
                             </div>
                         </section>
                     </main>
